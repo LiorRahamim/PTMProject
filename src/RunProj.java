03b47a5541b1ad73593773c7991adab14372e08e
Lior Rahamim <kenlelior@gmail.com>
2019-11-21 23:55:21 +0200

    put message to user

--- src/RunProj.java
+++ src/RunProj.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 
 public class RunProj {
 
 	public static void main(String[] args) {
-		System.out.println("dsfg");
-
+		System.out.println("Beginning of PTM course");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
 	}
 
 }
