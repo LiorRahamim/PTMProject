f1e0b8d5397a281aff4c41981287c944072c489b
Nadoiv <nadoiv@gmail.com>
2019-11-30 20:17:04 +0200

    new branch for creating server

--- src/server_side/MySerialServer.java
+++ src/server_side/MySerialServer.java
@@ -3,7 +3,7 @@ package server_side;
 public class MySerialServer implements Server {
 
 	@Override
-	public void open(int port, ClientHandler c) {
+	public void start(int port, ClientHandler c) {
 		// TODO Auto-generated method stub
 		
 	}
